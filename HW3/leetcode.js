4881d4afe48474c623451efbcf6b7c814e7f5859
InessaPopovich <inessa.popovich12@gmail.com>
2024-01-30 15:25:54 +0200

    tasks from leetcode

---
+++ HW3/leetcode.js
@@ -0,0 +1,40 @@
+
+//344. Reverse String
+// function rvrsString(s){
+// return s.reverse().join();
+// }
+
+// console.log(rvrsString(["h","e","l","l","o"])) //очікую в консоль o,l,l,e,h
+
+
+//1108. Defanging an IP Address
+// function string(adress){
+//     return adress.replace(/\./g, "[.]");
+//     }
+
+// console.log(string("1.1.1.1")) //очікую в консоль 1[.]1[.]1[.]1
+
+
+//2703. Return Length of Arguments Passed
+// function argumentsLength(args){
+//     return args.length;
+// }
+// console.log(argumentsLength([5,2,3,4])); //очікую в консоль 4
+
+
+//Examle of array1
+let massive = ["apple", "orange", "pineapple"];
+console.log(massive[2]); // очікую в консоль pineapple
+console.log(massive[1]); // очікую в консоль orange
+
+//Examle of array2
+let arr = [
+    { name: "ihor", age: 20, prof: "QA" },
+    { name: "vova", age: 23, prof: "footballer"}
+];
+console.log(arr[1]); //очікую в консоль { name: "vova", age: 23, prof: "footballer"}
+console.log(arr); //очікую все в консоль
+
+
+
+
